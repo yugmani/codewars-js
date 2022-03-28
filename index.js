d5c12d68a96f59e9d49040ea6f307836cc6c2e70
yugmani <yugmani@yahoo.com>
2022-03-27 23:30:00 -0700

    alternative solution

--- index.js
+++ index.js
@@ -170,18 +170,23 @@ console.log(descendingOrder(123456789)); //987654321
 
 // https://en.wikipedia.org/wiki/Factorial
 
-function factorial(n){
+function factorial(n) {
   //your code here
   let result = 1;
-  for(let i=n; i>0; i--){
+  for (let i = n; i > 0; i--) {
     result *= i;
   }
-  
+
   return result;
 }
 
-console.log(factorial(0));    //1
-console.log(factorial(1));    //1
-console.log(factorial(4));    //24
-console.log(factorial(7));    //5040
-console.log(factorial(17));   //355687428096000
+console.log(factorial(0)); //1
+console.log(factorial(1)); //1
+console.log(factorial(4)); //24
+console.log(factorial(7)); //5040
+console.log(factorial(17)); //355687428096000
+
+//alternative solution
+// function factorial(n){
+//   return n ? n * factorial(n-1) : 1;
+// }
