28a6bb6c47e55343e45a821ad17cf6d0d3e08b6e
yugmani <yugmani@yahoo.com>
2022-03-28 11:42:09 -0700

    small through check of each element in an array

--- index.js
+++ index.js
@@ -190,3 +190,16 @@ console.log(factorial(17)); //355687428096000
 // function factorial(n){
 //   return n ? n * factorial(n-1) : 1;
 // }
+
+
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+function smallEnough(a, limit){
+  return a.every(item=>item<=limit);
+}
+
+console.log(smallEnough([66, 101], 200));   //true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));  //false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); //true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));   //true
