05984a738a1b70b8a12b2ae9566a5ec1ddb1f3bf
yugmani <yugmani@yahoo.com>
2022-03-28 12:25:45 -0700

    Jaden Smith's tweet

--- index.js
+++ index.js
@@ -230,3 +230,52 @@ function smallEnough(array, limit){
     return array.filter( (value) => value > limit ).length > 0 ? false : true;
 }
 */
+
+// Jaden Smith Tweet
+// ******************************************
+
+/*
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+*/
+let str = "How can mirrors be real if our eyes aren't real";
+
+//General solution
+/*
+String.prototype.toJadenCase = function () {
+  //...
+
+  let result = str.split(' ');
+  result = result
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+  return result;
+};
+*/
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+};
+
+console.log(str.toJadenCase());
+
+// Alternative solution
+/*
+String.prototype.toJadenCase = function () {
+
+  function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  return this.split(' ').map(capitalizeFirstLetter).join(' ');
+};
+*/
